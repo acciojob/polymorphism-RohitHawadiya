0ef530c64a010a911cb23a5d00633d942321bb29
RohitHawadiya <rhtptdr001@gmail.com>
2023-03-15 15:53:45 +0530

    Assignment re-done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,7 @@ public class Main {
           Product p=new Product();
           int ans1=p.product(10,20);
           int ans2=p.product(10,20,30);
-          double ans3=p.product(10,20,30);
+          double ans3=p.product(10.0,20.0,30.0);
 
           System.out.println(ans1+" "+ans2+" "+ans3);
       }
